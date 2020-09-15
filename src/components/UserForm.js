b08dfb4e9b4cabfc4ecc11a67275dfeae1ba0925
Kokila-D <dhamakoki06@gmail.com>
2020-09-15 12:05:35 +0530

    first commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
+
+export class UserForm extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    phnum:'',
+    school:'',
+    clg:'',
+    cgpa:'',
+    occupation: '',
+    city: '',
+  };
+
+
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step + 1
+    });
+  };
+
+  
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({
+      step: step - 1
+    });
+  };
+
+  
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  };
+
+  render() {
+    const { step } = this.state;
+    const { firstName, lastName, email, phnum, school, clg, cgpa, occupation, city } = this.state;
+    const values = { firstName, lastName, email, phnum, school, clg, cgpa, occupation, city };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+      default:
+        (console.log('multi-step form built with React.'))
+    }
+  }
+}
+
+export default UserForm;
